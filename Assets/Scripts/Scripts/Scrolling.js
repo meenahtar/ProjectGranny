4b6af170c35a32283e11b2d76fd1c02ab4288faf
Jared <Jas1421@rit.edu>
2014-12-02 21:41:53 -0500

    -Stuff -First level start -Characters on screen

---
+++ Assets/Scripts/Scripts/Scrolling.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var Xpos : float;
+
+function Start () 
+{
+	Xpos = transform.position.x;
+}
+
+function Update () 
+{
+	if (Input.GetKey("g"))
+	{
+		transform.position.x -= .075;
+	}
+
+}
---
+++ Assets/Scripts/Scripts/changeTexture.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	var texture: Texture = Resources.Load("GrannyAttack");
+	
+	if (Input.GetKey("f"))
+	{
+		renderer.material.mainTexture = texture;
+	}
+}
