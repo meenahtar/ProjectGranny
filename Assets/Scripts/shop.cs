6035de0b4e4eb3ef3bdd67af84ae967b33124010
LunarStarStorm <rein.of.stars@gmail.com>
2014-12-08 10:51:33 -0500

    -Shop visual feedback working -Can get to shop though main menu (for testing only)

--- Assets/Scripts/shop.cs
+++ Assets/Scripts/shop.cs
@@ -11,6 +11,7 @@ public class shop : MonoBehaviour {
 	public bool item2Box = false;
 	public bool item3Box = false;
 	public bool item4Box = false;
+	public bool isContinueBTN = false;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +20,19 @@ public class shop : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (hasItem1 == true) {
+			markOff();
+		}
+		if(hasItem2 == true){
+			markOff();
+		}
+		if(hasItem3 == true){
+			markOff();
+		}
+		if(hasItem4 == true){
+			markOff();
+		}
+		//print ("Has item 1 = " + hasItem1 + " Has item 2 = " + hasItem2 + " Has item 3 = " + hasItem3 + " Has item 4 = " + hasItem4);
 	}
 
 	void OnMouseEnter(){
@@ -32,17 +45,25 @@ public class shop : MonoBehaviour {
 
 	void OnMouseUp(){
 		if (item1Box == true) {
-			//renderer.material.color = color.black;
-			//GUI.Box(Rect(0,0,Screen.width, Screen.height), "Got it");
+			hasItem1 = true;
 		}
 		if (item2Box == true) {
-			//renderer.material.color = color.black;
+			hasItem2 = true;
 		}
 		if (item3Box == true) {
-			//renderer.material.color = color.black;
+			hasItem3 = true;
 		}
 		if (item4Box == true) {
-			//renderer.material.color = color.black;
+			hasItem4 = true;
 		}
+		if (isContinueBTN == true) {
+			Application.LoadLevel("mainMenu");
+		}
+
+		//print ("Has item 1 = " + hasItem1 + " Has item 2 = " + hasItem2 + " Has item 3 = " + hasItem3 + " Has item 4 = " + hasItem4);
+	}
+
+	void markOff(){
+		renderer.material.color = Color.green;
 	}
 }
